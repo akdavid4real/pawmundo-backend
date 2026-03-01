0ea184a14e599d5567d5d6fc5baaa7e960e53150
akdavid <akdavid4real@gmail.com>
2026-03-01 16:59:14 +0100

    feat: Add Vercel environment variables and a Prisma script to query users.

---
+++ check-users.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const users = await prisma.user.findMany({ select: { email: true, password: true } });
+    console.log(users);
+}
+
+main()
+    .catch(e => console.error(e))
+    .finally(async () => await prisma.$disconnect());
